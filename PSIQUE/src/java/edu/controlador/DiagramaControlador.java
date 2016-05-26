872d1c4c1d63ce9235a6bf98fd031b515cac47a3
DavidVargasZuluaga <DavidBrootal@10.1.16.152>
2016-05-26 11:24:58 -0500

    Actualizaciones Diagrama Familiar

--- PSIQUE/src/java/edu/controlador/DiagramaControlador.java
+++ PSIQUE/src/java/edu/controlador/DiagramaControlador.java
@@ -1,81 +1,149 @@
 package edu.controlador;
- 
+
+import edu.entidad.*;
+import edu.fachada.*;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import javax.annotation.PostConstruct;
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
-import org.primefaces.model.diagram.DefaultDiagramModel;
-import org.primefaces.model.diagram.DiagramModel;
-import org.primefaces.model.diagram.Element;
-import org.primefaces.model.diagram.Connection;
-import org.primefaces.model.diagram.connector.FlowChartConnector;
-import org.primefaces.model.diagram.endpoint.BlankEndPoint;
-import org.primefaces.model.diagram.endpoint.EndPoint;
-import org.primefaces.model.diagram.endpoint.EndPointAnchor;
-import org.primefaces.model.diagram.overlay.ArrowOverlay;
-import org.primefaces.model.diagram.overlay.LabelOverlay;
- 
-@ManagedBean(name = "diagramFlowChartView")
-@RequestScoped
-public class DiagramaControlador {
-     
-    private DefaultDiagramModel model;
- 
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ *
+ * @author DavidBrootal
+ */
+@Named(value = "diagramaControlador")
+@SessionScoped
+public class DiagramaControlador implements Serializable {
+
+    @Inject
+    FamiliarFacade familiarFacade;
+
+    @Inject
+    FamiliarrelacionFacade familiarrelacionFacade;
+
+    List<Familiar> padres;
+    List<Familiar> hijos;
+    List<Familiar> parejas;
+    List<Familiar> todosFamiliars;
+
+    private int modalModificado;
+
     @PostConstruct
     public void init() {
-        model = new DefaultDiagramModel();
-        model.setMaxConnections(-1);
-         
-        FlowChartConnector connector = new FlowChartConnector();
-        connector.setPaintStyle("{strokeStyle:'#C7B097',lineWidth:3}");
-        model.setDefaultConnector(connector);
-         
-        Element start = new Element("Fight for your dream", "20em", "6em");
-        start.addEndPoint(new BlankEndPoint(EndPointAnchor.BOTTOM));
-        start.addEndPoint(new BlankEndPoint(EndPointAnchor.LEFT));
-         
-        Element trouble = new Element("Do you meet some trouble?", "20em", "18em");
-        trouble.addEndPoint(new BlankEndPoint(EndPointAnchor.TOP));
-        trouble.addEndPoint(new BlankEndPoint(EndPointAnchor.BOTTOM));
-        trouble.addEndPoint(new BlankEndPoint(EndPointAnchor.RIGHT));
-         
-        Element giveup = new Element("Do you give up?", "20em", "30em");
-        giveup.addEndPoint(new BlankEndPoint(EndPointAnchor.TOP));
-        giveup.addEndPoint(new BlankEndPoint(EndPointAnchor.LEFT));
-        giveup.addEndPoint(new BlankEndPoint(EndPointAnchor.RIGHT));
-         
-        Element succeed = new Element("Succeed", "50em", "18em");
-        succeed.addEndPoint(new BlankEndPoint(EndPointAnchor.LEFT));
-        succeed.setStyleClass("ui-diagram-success");
-         
-        Element fail = new Element("Fail", "50em", "30em");
-        fail.addEndPoint(new BlankEndPoint(EndPointAnchor.LEFT));
-        fail.setStyleClass("ui-diagram-fail");
-                         
-        model.addElement(start);
-        model.addElement(trouble);
-        model.addElement(giveup);
-        model.addElement(succeed);
-        model.addElement(fail);
-                 
-        model.connect(createConnection(start.getEndPoints().get(0), trouble.getEndPoints().get(0), null));
-        model.connect(createConnection(trouble.getEndPoints().get(1), giveup.getEndPoints().get(0), "Yes"));
-        model.connect(createConnection(giveup.getEndPoints().get(1), start.getEndPoints().get(1), "No"));
-        model.connect(createConnection(trouble.getEndPoints().get(2), succeed.getEndPoints().get(0), "No"));
-        model.connect(createConnection(giveup.getEndPoints().get(2), fail.getEndPoints().get(0), "Yes"));
+        modalModificado = 0;
+        padres = new ArrayList();
+        hijos = new ArrayList();
+        parejas = new ArrayList();
+        todosFamiliars = familiarFacade.findAll();
     }
-     
-    public DiagramModel getModel() {
-        return model;
-    }
-     
-    private Connection createConnection(EndPoint from, EndPoint to, String label) {
-        Connection conn = new Connection(from, to);
-        conn.getOverlays().add(new ArrowOverlay(20, 20, 1, 1));
-         
-        if(label != null) {
-            conn.getOverlays().add(new LabelOverlay(label, "flow-label", 0.5));
+
+    public String cargarFamiliaresCercanos(Aprendiz a) {
+        init();
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        ExternalContext externalContext = facesContext.getExternalContext();
+        HttpServletRequest httpServletRequest = (HttpServletRequest) facesContext.getExternalContext().getRequest();
+        List<Familiar> todosFamiliares = familiarFacade.findAll();
+        try {
+            for (int i = 0; i < todosFamiliares.size(); i++) {
+                if (todosFamiliares.get(i).getIdAprendiz().equals(a)) {
+                    if (!todosFamiliares.get(i).getParentezco().isEmpty()) {
+                        switch (todosFamiliares.get(i).getParentezco()) {
+                            case "PADRE":
+                                padres.add(todosFamiliares.get(i));
+                                break;
+                            case "PAREJA":
+                                parejas.add(todosFamiliares.get(i));
+                                break;
+                            case "HIJO":
+                                hijos.add(todosFamiliares.get(i));
+                                break;
+                        }
+                    }
+                }
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-         
-        return conn;
+        return "perfilAprendiz.xhtml";
+
     }
+
+    public List<Familiar> cargarRelaciones(Familiar f) {
+        List<Familiarrelacion> todasRelaciones = familiarrelacionFacade.findAll();
+        List<Familiar> todosFamiliares = familiarFacade.findAll();
+        List<Familiar> familiares = new ArrayList();
+        try {
+            for (int i = 0; i < todasRelaciones.size(); i++) {
+                if (todasRelaciones.get(i).getIdFamiliarParentezco().equals(f)) {
+                    familiares.add(todasRelaciones.get(i).getIdFamiliarRelacion());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return familiares;
+    }
+
+    public String editarFamiliar(Familiar f) {
+        String res = "perfilAprendiz.xhtml";
+        try {
+            familiarFacade.edit(f);
+            modalModificado = 1;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return res;
+    }
+
+    public void cerrarModal() {
+        modalModificado = 0;
+    }
+
+    public List<Familiar> getPadres() {
+        return padres;
+    }
+
+    public void setPadres(List<Familiar> padres) {
+        this.padres = padres;
+    }
+
+    public List<Familiar> getHijos() {
+        return hijos;
+    }
+
+    public void setHijos(List<Familiar> hijos) {
+        this.hijos = hijos;
+    }
+
+    public List<Familiar> getParejas() {
+        return parejas;
+    }
+
+    public void setParejas(List<Familiar> parejas) {
+        this.parejas = parejas;
+    }
+
+    public List<Familiar> getTodosFamiliars() {
+        return todosFamiliars;
+    }
+
+    public void setTodosFamiliars(List<Familiar> todosFamiliars) {
+        this.todosFamiliars = todosFamiliars;
+    }
+
+    public int getModalModificado() {
+        return modalModificado;
+    }
+
+    public void setModalModificado(int modalModificado) {
+        this.modalModificado = modalModificado;
+    }
+
 }
